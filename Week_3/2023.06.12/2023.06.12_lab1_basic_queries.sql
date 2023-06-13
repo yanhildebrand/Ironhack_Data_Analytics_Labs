2e33f41c5c2c09717275b4d221b5aac2ca1ef7cf
yanhildebrand <hildebrand.yan@gmail.com>
2023-06-13 11:32:37 +0200

    2023.06.12_4

--- Week_3/2023.06.12/2023.06.12_lab1_basic_queries.sql
+++ Week_3/2023.06.12/2023.06.12_lab1_basic_queries.sql
@@ -24,29 +24,31 @@ select distinct release_year from sakila.film;
 
 # 5. Counting records for database insights:
 # 5.1 Determine the number of stores that the company has.
-select count(store_id) from sakila.store;
+select count(store_id) from store;
 
 # 5.2 Determine the number of employees that the company has.
-select count(staff_id) from sakila.staff;
+select count(staff_id) from staff;
 
 # 5.3 Determine how many films are available for rent and how many have been rented.
-select count(distinct(inventory_id)) from sakila.inventory;
-select count(rental_id) from sakila.rental;
+select count(distinct(inventory_id)) from inventory;
+select count(rental_id) from rental;
 
 # 5.4 Determine the number of distinct last names of the actors in the database.
-select * from sakila.actor;
-select count(distinct(last_name)) from sakila.actor;
+select * from actor;
+select count(distinct(last_name)) from actor;
 
 # 6. Retrieve the 10 longest films.
-select * from sakila.film;
-select max(length) from sakila.film;
-select * from sakila.film order by length desc limit 10;
+select * from film;
+select max(length) from film;
+select * from film order by length desc limit 10;
 
 # 7.1 Retrieve all actors with the first name "SCARLETT".
-select * from sakila.actor where first_name in ("SCARLETT");
+select * from actor where first_name in ("SCARLETT");
+select * from actor where first_name = "SCARLETT";
+select * from actor where first_name like "SCARLETT";
 
 # 7.2 Retrieve all movies that have ARMAGEDDON in their title and have a duration longer than 100 minutes.
-select * from sakila.film where title like '%ARMAGEDDON%' and length > 100;
+select * from film where title like '%ARMAGEDDON%' and length > 100;
 
 # 7.3 Determine the number of films that include Behind the Scenes content
-select count(*) from sakila.film where special_features like '%behind%'
+select count(*) from film where special_features like '%behind%'
--- Week_3/2023.06.12/2023.06.12_lab2_data_aggregation_transformation.sql
+++ Week_3/2023.06.12/2023.06.12_lab2_data_aggregation_transformation.sql
@@ -4,20 +4,20 @@ show tables;
 
 ### Challenge 1
 # 1.1 Determine the shortest and longest movie durations and name the values as max_duration and min_duration.
-select max(length) as max_duration from sakila.film;
-select min(length) as min_duration from sakila.film;
+select max(length) as max_duration from film;
+select min(length) as min_duration from film;
 
 # 1.2. Express the average movie duration in hours and minutes. Don't use decimals. Hint: look for floor and round functions.
-select floor(avg(length)) as average_duration from sakila.film;
-select round(avg(length)) as average_duration from sakila.film;
-select concat(floor(avg(length)/60),'h ', mod(floor(avg(length)),60),'m') as average_duration from sakila.film;
+select floor(avg(length)) as average_duration from film;
+select round(avg(length)) as average_duration from film;
+select concat(floor(avg(length)/60),'h ', mod(floor(avg(length)),60),'m') as average_duration from film;
 
 # 2.1 Calculate the number of days that the company has been operating. Hint: To do this, use the rental table, and the DATEDIFF() function to subtract the earliest date in the rental_date column from the latest date.
-select * from sakila.rental;
-select DATEDIFF(max(rental_date), min(rental_date)) as days_active from sakila.rental;
+select * from rental;
+select DATEDIFF(max(rental_date), min(rental_date)) as days_active from rental;
 
 # 2.2 Retrieve rental information and add two additional columns to show the month and weekday of the rental. Return 20 rows of results.
-SELECT *, monthname(rental_date) as Month, dayname(rental_date) as Day from sakila.rental limit 20;
+SELECT *, monthname(rental_date) as Month, dayname(rental_date) as Day from rental limit 20;
 
 # 2.3 Retrieve rental information and add an additional column called DAY_TYPE with values 'weekend' or 'workday', depending on the day of the week. Hint: use a conditional expression.
 SELECT *,
@@ -34,8 +34,26 @@ SELECT title,
 	FROM rental
 	inner join inventory ON rental.inventory_id = inventory.inventory_id
 	inner join film ON inventory.film_id = film.film_id;
-    
+
 # 4. As a marketing team for a movie rental company, we need to create a personalized email campaign for our customers. To achieve this, we want to retrieve the concatenated first and last names of our customers, along with the first 3 characters of their email address, so that we can address them by their first name and use their email address to send personalized recommendations. The results should be ordered by last name in ascending order to make it easier for us to use the data.
-SELECT first_name, last_name, email as email_1 from sakila.customer;
+SELECT CONCAT(`first_name`, ' ', `last_name`) as "Name", LEFT(email, 3) as emailShort from sakila.customer;
 
 # Challenge 2
+# 1.1 The total number of films that have been released.
+select count(title) from film;
+
+# 1.2 The number of films for each rating.
+select rating, count(rating) as number from film group by rating;
+
+# 1.3 The number of films for each rating, and sort the results in descending order of the number of films. This will help us better understand the popularity of different film ratings and adjust our purchasing decisions accordingly.
+select rating, count(rating) as number from film group by rating order by count(rating) desc;
+
+# 2. We need to track the performance of our employees. Using the rental table, determine the number of rentals processed by each employee. This will help us identify our top-performing employees and areas where additional training may be necessary. 
+select count(staff_id) from rental group by staff_id;
+
+# Using the film table, determine:
+# 3.1 The mean film duration for each rating, and sort the results in descending order of the mean duration. Round off the average lengths to two decimal places. This will help us identify popular movie lengths for each category.
+select rating, avg(length) as "average_length" from film group by rating order by average_length desc;
+
+# 3.2 Identify which ratings have a mean duration of over two hours, to help us select films for customers who prefer longer movies. Determine which last names are not repeated in the table actor.
+select rating, avg(length) as "average_length" from film group by rating having average_length >= 120;
